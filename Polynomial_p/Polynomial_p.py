73b991cfb828841b42787149805e92a8561e7df4
sb1123 <sam.barrett23@imperial.ac.uk>
2024-10-15 10:29:36 +0100

    Initial Polynomial_p package implementation

---
+++ Polynomial_p/Polynomial_p.py
@@ -0,0 +1,6 @@
+class Polynomial_p: 
+    def __init__(self,coefs):
+        # content of __init__ method takes\
+        # argument and sets up appropriate attributes.
+        self.coefficients = coefs 
+        
